f52b8f9f04449f38472ef21fd1bb3436eb574bb7
Jeanette Hitchcock <whatthefuture@gmail.com>
2024-12-19 20:16:52 -0500

    Update attributes.js

--- src/attributes.js
+++ src/attributes.js
@@ -1,7 +1,7 @@
 export const CANVAS_WIDTH = 500;
 export const CANVAS_HEIGHT = 800;
 export const showInitialLines = false;
-export const showIntersectingCircles = false;
+export const showIntersectingCircles = true;
 export const showConnectingLines = true;
 export const radius = 50;
 export const iterations = 100;
